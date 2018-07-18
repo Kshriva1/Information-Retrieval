e2eb0f409ed819da83575c8e0ab8a9bd38479630
Kundan Shrivastav <kundan0395@gmail.com>
2018-07-18 10:48:11 -0400

    Add PostingList

---
+++ PostingList.java
@@ -0,0 +1,21 @@
+public class PostingList {
+    private int docId;
+    private int termFrequency;
+
+    public int getDocId() {
+        return docId;
+    }
+
+    public void setDocId(int docId) {
+        this.docId = docId;
+    }
+
+    public int getTermFrequency() {
+        return termFrequency;
+    }
+
+    public void setTermFrequency(int termFrequency) {
+        this.termFrequency = termFrequency; 
+    }
+}
+
